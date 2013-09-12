09f9287ccb3bc3dc60c67f856c1e86e268a99aee
Burlen Loring <burlen.loring@gmail.com>
2013-09-12 14:56:59 -0700

    servers: update hopper build scripts

--- servers/hopper/builds/config-pv3-a-hopper.sh
+++ servers/hopper/builds/config-pv3-a-hopper.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-HDF5=/opt/cray/hdf5/1.8.7/gnu/46
-MESA=/usr/common/graphics/mesa/9.2.0/
+HDF5=/opt/cray/hdf5/1.8.8/gnu/47
+MESA=/usr/common/graphics/mesa/9.2.0/llvmpipe/
 PYTHON=/usr/common/usg/python/2.7.1
 BOOST=/usr/common/graphics/ParaView/boost_1_46_1
-COMP=/opt/gcc/4.7.1/bin
+COMP=/opt/gcc/4.7.2/bin
 
-MPT=/opt/cray/mpt/5.6.0/gni/mpich2-gnu/47/lib
+MPT=/opt/cray/mpt/5.6.4/gni/mpich2-gnu/47/lib
 RCA=/opt/cray/rca/1.0.0-2.0401.38656.2.2.gem/lib64
 XPMEM=/opt/cray/xpmem/0.1-2.0401.36790.4.3.gem/lib64
 DMAPP=/opt/cray/dmapp/3.2.1-1.0401.5983.4.5.gem/lib64
-PMI=/opt/cray/pmi/4.0.0-1.0000.9282.69.4.gem/lib64
+PMI=/opt/cray/pmi/4.0.1-1.0000.9421.73.3.gem/lib64
 UGNI=/opt/cray/ugni/4.0-1.0401.5928.9.5.gem/lib64
 UDREG=/opt/cray/udreg/2.3.2-1.0401.5929.3.3.gem/lib64
-SCI=/opt/cray/libsci/12.0.00/GNU/47/mc12/lib
-SMA=/opt/cray/mpt/5.6.0/gni/sma/lib64
-SMA64=/opt/cray/mpt/5.6.0/gni/sma64/lib64
+SMA=/opt/cray/mpt/5.6.4/gni/sma/lib64
+SMA64=/opt/cray/mpt/5.6.4/gni/sma64/lib64
 ALPS=/usr/lib/alps
 
 echo
@@ -33,7 +32,7 @@ cmake \
     -DCMAKE_CXX_COMPILER=$COMP/g++ \
     -DCMAKE_EXE_LINKER=$COMP/g++ \
     -DBUILD_SHARED_LIBS=OFF \
-    -DPARAVIEW_ENABLE_PYTHON=ON \
+    -DPARAVIEW_ENABLE_PYTHON=OFF \
     -DPYTHON_EXECUTABLE=$PYTHON/bin/python \
     -DPYTHON_INCLUDE_DIR=$PYTHON/include/python2.7 \
     -DPYTHON_LIBRARY=$PYTHON/lib/libpython2.7.a \
@@ -52,7 +51,7 @@ cmake \
     -DMPI_CXX_COMPILER=$COMP/g++ \
     -DMPI_C_COMPILER=$COMP/gcc \
     -DMPI_LIBRARY=$MPT/libmpich_gnu_47.a \
-    -DMPI_EXTRA_LIBRARY=/usr/lib64/librt.a\;/usr/lib64/libpthread.a\;$SMA/libsma.a\;$XPMEM/libxpmem.a\;$DMAPP/libdmapp.a\;$UGNI/libugni.a\;$PMI/libpmi.a\;$ALPS/libalpslli.a\;$ALPS/libalpsutil.a\;$UDREG/libudreg.a\;$MPT/libmpl.a\;$MPT/libmpichcxx_gnu_47.a\; \
+    -DMPI_EXTRA_LIBRARY=$MPT/libmpichcxx_gnu_47.a\;$MPT/lib/mpichcxx.a\;$MPT/libmpich.a\;$SMA/libsma.a\;$XPMEM/libxpmem.a\;$DMAPP/libdmapp.a\;$UGNI/libugni.a\;$PMI/libpmi.a\;$ALPS/libalpslli.a\;$ALPS/libalpsutil.a\;$UDREG/libudreg.a\;$MPT/libmpl.a\;/usr/lib64/librt.a\;/usr/lib64/libpthread.a\;/usr/lib64/libc.a\; \
     -DMPI_INCLUDE_PATH=$MPT/../include \
     -DMPIEXEC=/usr/common/usg/altd/1.0/bin/aprun \
     -DBOOST_ROOT=$BOOST \
