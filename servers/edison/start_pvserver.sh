536756ed3144f2bcae800304ef258da64569b5ec
Burlen Loring <burlen.loring@gmail.com>
2014-06-25 12:46:13 -0700

    servers: edison added logic to handle requested install that's not present

--- servers/edison/start_pvserver.sh
+++ servers/edison/start_pvserver.sh
@@ -30,25 +30,81 @@ ACCOUNT=$4
 QUEUE=$5
 PORT=$6
 PV_VER=$7
-USE_PY=$8
+LINK=$8
 
 # if python support requested then use the DSO install
 # else use the static linked install, it starts much faster.
 # new pv installs must have both DSO and static
-PV_LINK=shared
-if [[ "$USE_PY" == "0" ]]
+HAVE_STATIC=0
+HAVE_SHARED=0
+HAVE_HYBRID=0
+if [[ -e /usr/common/graphics/ParaView/$PV_VER/static/start_pvserver.sh ]]
 then
-  PV_LINK=static
+  HAVE_STATIC=1
+fi
+if [[ -e /usr/common/graphics/ParaView/$PV_VER/shared/start_pvserver.sh ]]
+then
+  HAVE_SHARED=1
+fi
+if [[ -e /usr/common/graphics/ParaView/$PV_VER/hybrid/start_pvserver.sh ]]
+then
+  HAVE_HYBRID=1
+fi
+# to support old installs that don't have all the link types
+if [[ "$LINK" == "0" ]]
+then
+  if [[ "$HAVE_STATIC" == "1" ]]
+  then
+    PV_LINK=static
+  elif [[ "$HAVE_HYBRID" == "1" ]]
+  then
+    PV_LINK=hybrid
+  elif [[ "$HAVE_SHARED" == "1" ]]
+  then
+    PV_LINK=shared
+  else
+    PV_LINK=none
+  fi
+fi
+if [[ "$LINK" == "1" ]]
+then
+  if [[ "$HAVE_SHARED" == "1" ]]
+  then
+    PV_LINK=shared
+  elif [[ "$HAVE_HYBRID" == "1" ]]
+  then
+    PV_LINK=hybrid
+  elif [[ "$HAVE_STATIC" == "1" ]]
+  then
+    PV_LINK=static
+  else
+    PV_LINK=none
+  fi
+fi
+if [[ "$LINK" == "2" ]]
+then
+  if [[ "$HAVE_HYBRID" == "1" ]]
+  then
+    PV_LINK=hybrid
+  elif [[ "$HAVE_SHARED" == "1" ]]
+  then
+    PV_LINK=shared
+  elif [[ "$HAVE_STATIC" == "1" ]]
+  then
+    PV_LINK=static
+  else
+    PV_LINK=none
+  fi
 fi
 
 # this is the recommended version to use on NERSC edison
 # when new PV is installed this value needs to be updated
-NERSC_PV_VER=4.0.1
+NERSC_PV_VER=4.1.0
 if [[ "$PV_VER" != "$NERSC_PV_VER" ]]
 then
   echo\
     "WARNING: You're using ParaView ver. $PV_VER. The recommended "\
-    "version is ParaView ver. $NERSC_PV"
+    "version is ParaView ver. $NERSC_PV_VER"
 fi
 
 if [[ ! (-e /usr/common/graphics/ParaView/$PV_VER/$PV_LINK/start_pvserver.sh) ]]
@@ -57,7 +113,7 @@ then
 
   echo
   echo\
-    "Error: Version $PV_VER($PV_LINK) is not installed. Client and server "\
+    "Error: Version $PV_VER ($PV_LINK) is not installed. Client and server "\
     "versions must match. You will need to download and install a client "\
     "binary for one of the following installed versions from www.paraview.org "\
     "and try again."
